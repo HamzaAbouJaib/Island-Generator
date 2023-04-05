79410ad282aba60a9ca921f48fef25e490e7c5d9
HamzaAbouJaib <hamza.aboujaib.0303@gmail.com>
2023-04-05 19:46:20 -0400

    Create an interface to define the public contract of finding a path between two nodes, resolve #4

--- pathfinder/src/main/java/pathfinder/PathFinder.java
+++ pathfinder/src/main/java/pathfinder/PathFinder.java
@@ -1,10 +1,8 @@
 package pathfinder;
 
-import pathfinder.graphadt.Edge;
 import pathfinder.graphadt.Node;
-
 import java.util.List;
 
 public interface PathFinder {
-    public List<Edge> findPath(Node n1, Node n2);
+    List<Node> findPath(Node n1, Node n2);
 }
