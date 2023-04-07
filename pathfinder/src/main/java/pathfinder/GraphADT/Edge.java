c15988cb21c7eb962567724b93a11426941d08c9
HamzaAbouJaib <hamza.aboujaib.0303@gmail.com>
2023-04-07 16:07:05 -0400

    Add new constructor to create an edge with default weight of 1, update F45

--- pathfinder/src/main/java/pathfinder/GraphADT/Edge.java
+++ pathfinder/src/main/java/pathfinder/GraphADT/Edge.java
@@ -6,6 +6,13 @@ public class Edge {
     private Node n2;
     private double weight;
 
+
+    public Edge(Node n1, Node n2) {
+        this.n1 = n1;
+        this.n2 = n2;
+        this.weight = 1;
+    }
+
     public Edge(Node n1, Node n2, double weight) {
         this.n1 = n1;
         this.n2 = n2;
