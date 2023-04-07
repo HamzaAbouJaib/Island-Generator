28c8657734686549b2e0becfc1e624eedbab076f
HamzaAbouJaib <hamza.aboujaib.0303@gmail.com>
2023-04-06 23:34:47 -0400

    Remove unused imports

--- pathfinder/src/main/java/pathfinder/PathFinder.java
+++ pathfinder/src/main/java/pathfinder/PathFinder.java
@@ -1,10 +1,8 @@
 package pathfinder;
 
-import pathfinder.graphadt.Edge;
 import pathfinder.graphadt.Graph;
 import pathfinder.graphadt.Node;
 import java.util.List;
-import java.util.Map;
 
 public interface PathFinder {
     List<Node> findPath(Graph g, Node n1, Node n2);
