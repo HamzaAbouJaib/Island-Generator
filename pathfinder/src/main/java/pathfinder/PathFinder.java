46023d26273859b7d48d65a45e70d829761cf78f
HamzaAbouJaib <hamza.aboujaib.0303@gmail.com>
2023-04-06 23:32:30 -0400

    Update PathFinder interface to accept a graph as input, resolve #4

--- pathfinder/src/main/java/pathfinder/PathFinder.java
+++ pathfinder/src/main/java/pathfinder/PathFinder.java
@@ -1,10 +1,11 @@
 package pathfinder;
 
 import pathfinder.graphadt.Edge;
+import pathfinder.graphadt.Graph;
 import pathfinder.graphadt.Node;
 import java.util.List;
 import java.util.Map;
 
 public interface PathFinder {
-    List<Node> findPath(Map<Node, List<Edge>> g, Node n1, Node n2);
+    List<Node> findPath(Graph g, Node n1, Node n2);
 }
